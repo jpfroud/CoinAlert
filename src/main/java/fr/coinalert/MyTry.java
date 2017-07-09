44d5892601898cf4e50462a862df7fe3c6dde57d
Jean-Pierre Froud <jpfroud@gmail.com>
2017-07-09 11:39:52 +0200

    email sending

--- src/main/java/fr/coinalert/MyTry.java
+++ src/main/java/fr/coinalert/MyTry.java
@@ -4,12 +4,18 @@ import java.io.IOException;
 import java.math.BigDecimal;
 import java.math.RoundingMode;
 
+import javax.mail.Message;
+
 import org.knowm.xchange.Exchange;
 import org.knowm.xchange.ExchangeFactory;
 import org.knowm.xchange.currency.CurrencyPair;
 import org.knowm.xchange.kraken.KrakenExchange;
 import org.knowm.xchange.kraken.dto.marketdata.KrakenTicker;
 import org.knowm.xchange.kraken.service.KrakenMarketDataServiceRaw;
+import org.simplejavamail.email.Email;
+import org.simplejavamail.mailer.Mailer;
+import org.simplejavamail.mailer.config.ServerConfig;
+import org.simplejavamail.mailer.config.TransportStrategy;
 
 public class MyTry {
 
@@ -22,32 +28,54 @@ public class MyTry {
 				.getMarketDataService();
 
 		// Get the latest ticker data showing BTC to EUR
-		KrakenTicker ticker = krakenMarketDataService.getKrakenTicker(CurrencyPair.ETH_EUR);
+		CurrencyPair pair = CurrencyPair.ETH_EUR;
+		KrakenTicker ticker = krakenMarketDataService.getKrakenTicker(pair);
 		BigDecimal base = ticker.getClose().getPrice();
 		BigDecimal before = base;
-		BigDecimal percent = BigDecimal.valueOf(0);
-		BigDecimal plus3percent = increase(base, percent);
-		BigDecimal minus3percent = decrease(base, percent);
+		BigDecimal percent = BigDecimal.valueOf(0.03);
+		BigDecimal plusPercent = increase(base, percent);
+		BigDecimal minusPercent = decrease(base, percent);
 
 		while (true) {
 			// Get the latest ticker data showing BTC to EUR
-			ticker = krakenMarketDataService.getKrakenTicker(CurrencyPair.ETH_EUR);
+			ticker = krakenMarketDataService.getKrakenTicker(pair);
 			BigDecimal close = ticker.getClose().getPrice();
-			if (greaterThan(close, plus3percent) || lesserThan(close, minus3percent)) {
+			if (greaterThan(close, plusPercent) || lesserThan(close, minusPercent)) {
 				base = close;
-				plus3percent = increase(base, percent);
-				minus3percent = decrease(base, percent);
+				plusPercent = increase(base, percent);
+				minusPercent = decrease(base, percent);
 				BigDecimal variation = getVariation(before, close);
-				System.out.println(variation + "% !!");
-				System.out.println("before = " + before + " - current = " + close);
-				
+
 				// send mail
-				
-				
+				sendMail(pair, before, close, variation);
+
 				before = close;
 			}
-			Thread.sleep(5000);
+			Thread.sleep(10000);
 		}
+	}
+
+	private static void sendMail(CurrencyPair pair, BigDecimal before, BigDecimal close, BigDecimal variation) {
+		String subject = "Update on " + pair; 
+		String text = "before = " + before + " - current = " + close;
+		String host = Messages.getString("MyTry.mail.host"); 
+		String username = Messages.getString("MyTry.mail.username"); 
+		String password = Messages.getString("MyTry.mail.password"); 
+		String rep1 = Messages.getString("MyTry.recipient.name"); 
+		String rep1Adress = Messages.getString("MyTry.recipient.email"); 
+
+		System.out.println(variation);
+		System.out.println(text);
+
+		Email email = new Email();
+		email.setFromAddress(Messages.getString("MyTry.sender.name"), username); 
+		email.setReplyToAddress("Noreply", "noreply@null.com");
+		email.addRecipient(rep1, rep1Adress, Message.RecipientType.TO);
+		email.setSubject(subject);
+		email.setText(variation + "%\n" + text);
+
+		new Mailer(new ServerConfig(host, 25, username, password), TransportStrategy.SMTP_PLAIN).sendMail(email);
+
 	}
 
 	private static BigDecimal getVariation(BigDecimal before, BigDecimal after) {
