b348e25abc776e379b6d4a4db48090249ae36b00
Jean-Pierre Froud <jpfroud@gmail.com>
2017-07-10 17:55:32 +0200

    Retry on error

--- src/main/java/fr/coinalert/CoinWatcher.java
+++ src/main/java/fr/coinalert/CoinWatcher.java
@@ -1,5 +1,6 @@
 package fr.coinalert;
 
+import java.io.IOException;
 import java.io.PrintWriter;
 import java.io.StringWriter;
 import java.math.BigDecimal;
@@ -22,25 +23,35 @@ final class CoinWatcher extends Thread {
 
 	@Override
 	public void run() {
-		try {
-			// Get the latest ticker data
-			KrakenTicker ticker = krakenMarketDataService.getKrakenTicker(pair);
-			BigDecimal base = ticker.getClose().getPrice();
-			BigDecimal before = base;
-			BigDecimal percent = new BigDecimal(Messages.getString("CoinAlert.percent"));
-			BigDecimal plusPercent = BigDecimalUtil.increase(base, percent);
-			BigDecimal minusPercent = BigDecimalUtil.decrease(base, percent);
-			Trend trend = new Trend();
-			while (true) {
+		// Get the latest ticker data
+		boolean initOK = false;
+		KrakenTicker ticker = null;
+		do {
+			try {
+				ticker = krakenMarketDataService.getKrakenTicker(pair);
+				initOK = true;
+			} catch (IOException e) {
+				e.printStackTrace();
+				continue;
+			}
+		} while (!initOK);
+		BigDecimal base = ticker.getClose().getPrice();
+		BigDecimal before = base;
+		BigDecimal percent = new BigDecimal(Messages.getString("CoinAlert.percent"));
+		BigDecimal plusPercent = BigDecimalUtil.increase(base, percent);
+		BigDecimal minusPercent = BigDecimalUtil.decrease(base, percent);
+		Trend trend = new Trend();
+		while (true) {
+			try {
 				// Get the latest ticker data
 				ticker = krakenMarketDataService.getKrakenTicker(pair);
 				BigDecimal close = ticker.getClose().getPrice();
-				System.out.println("Last " + pair +" : " + close);
+				System.out.println("Last " + pair + " : " + close);
 				if (BigDecimalUtil.greaterThan(close, plusPercent) || BigDecimalUtil.lesserThan(close, minusPercent)) {
 					BigDecimal variation = BigDecimalUtil.getVariation(before, close);
 					trend.update(variation);
 					base = close;
-					
+
 					plusPercent = BigDecimalUtil.increase(base, trend.getTopPercent());
 					minusPercent = BigDecimalUtil.decrease(base, trend.getBottomPercent());
 
@@ -50,18 +61,21 @@ final class CoinWatcher extends Thread {
 					before = close;
 				}
 				Thread.sleep(5000);
-			}
-		} catch (Exception e) {
-			e.printStackTrace();
-			StringWriter errors = new StringWriter();
-			e.printStackTrace(new PrintWriter(errors));
-			MailSender.sendMail("CoinAlert exception", errors.toString(), new Recipient(Messages.getString("CoinAlert.admin.name"),
-					Messages.getString("CoinAlert.admin.email"), Message.RecipientType.TO));
-			// wait 5 minutes
-			try {
-				Thread.sleep(300000);
-			} catch (InterruptedException e1) {
-				e1.printStackTrace();
+
+			} catch (Exception e) {
+				e.printStackTrace();
+				StringWriter errors = new StringWriter();
+				e.printStackTrace(new PrintWriter(errors));
+				MailSender.sendMail("CoinAlert exception", errors.toString(),
+						new Recipient(Messages.getString("CoinAlert.admin.name"),
+								Messages.getString("CoinAlert.admin.email"), Message.RecipientType.TO));
+				// wait 5 sec
+				try {
+					Thread.sleep(5000);
+				} catch (InterruptedException e1) {
+					e1.printStackTrace();
+				}
+				continue;
 			}
 		}
 	}
@@ -69,7 +83,7 @@ final class CoinWatcher extends Thread {
 	static void sendAlert(CurrencyPair pair, BigDecimal before, BigDecimal close, BigDecimal variation, Trend trend) {
 		String subject = "Update on " + pair;
 		String text = pair + " - before = " + before + " - current = " + close;
-		String emailText = variation + "%\n" + text+ "%\n" + trend.getMessage();
+		String emailText = variation + "%\n" + text + "%\n" + trend.getMessage();
 		System.out.println(variation);
 		System.out.println(text);
 		System.out.println(trend.getMessage());
